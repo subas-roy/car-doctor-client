3cfcf42fe1c85d3b68b1729c0a26bb34764da38c
subas-roy <subasroy46@gmail.com>
2024-10-26 07:36:11 +0600

    Project Setup And Set Basic Layout For Footer And Header

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './routes/Routes';
+
+createRoot(document.getElementById('root')).render(
+  <div className='max-w-7xl mx-auto'>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  </div>
+)
